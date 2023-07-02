fc64200cf6a0feedef9fb46386b7203bbf2e86ac
Documenter.jl <documenter@juliadocs.github.io>
2023-07-02 21:37:08 +0000

    build based on c8dab51

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Effectful","category":"page"},{"location":"#Effectful","page":"Home","title":"Effectful","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Effectful.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Effectful]","category":"page"}]
+}
